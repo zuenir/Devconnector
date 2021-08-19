a9872a2076e656bd48e81e97b8b095d474e8d6be
zuenir <zuenirlima@gmail.com>
2021-08-19 11:58:41 -0300

    #Update - App, Dashboard, PrivateRoute

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,13 +1,9 @@
 import React, { useEffect } from 'react'
 import PropTypes from 'prop-types'
 import {connect} from 'react-redux';
-import {getCurrentProfile} from '../../actions/profile';
+//import {getCurrentProfile} from '../../actions/profile';
 
-const Dashboard = ({getCurrentProfile, auth, profile}) => {
-    useEffect(() => {
-        getCurrentProfile();
-    }, []);
- 
+const Dashboard = props => {
     return (
         <div>
             Dashboard
@@ -15,15 +11,6 @@ const Dashboard = ({getCurrentProfile, auth, profile}) => {
     )
 }
 
-Dashboard.propTypes = {
-    getCurrentProfile: PropTypes.func.isRequired,
-    auth: PropTypes.object.isRequired,
-    profile: PropTypes.object.isRequired,
-}
+Dashboard.propTypes = {}
 
-const mapStateToProps = state => ({
-    auth: state.auth,
-    profile: state.profile
-});
-
-export default connect(mapStateToProps, {getCurrentProfile})(Dashboard);
+export default Dashboard;
